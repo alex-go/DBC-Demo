a923db61fb6ad539223785849327932ad07dd0fd
Alex <alex-go@users.noreply.github.com>
2018-02-20 16:27:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -60,6 +60,7 @@ make_task_def(){
 
 push_ecr_image(){
 	eval $(aws ecr get-login --region us-east-1 --no-include-email)
+	echo "Pushing image"
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/dbc/demo1:$CIRCLE_SHA1
 }
 
